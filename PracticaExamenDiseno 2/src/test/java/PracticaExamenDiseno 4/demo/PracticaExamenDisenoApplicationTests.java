5dc23238983fb31a6063864453e5b599ff9c1b2a
Pazfs07 <124477702+Pazfs07@users.noreply.github.com>
2023-03-02 13:23:49 -0600

    Add files via upload

---
+++ PracticaExamenDiseno 2/src/test/java/PracticaExamenDiseno 4/demo/PracticaExamenDisenoApplicationTests.java
@@ -0,0 +1,13 @@
+package PracticaExamenDiseno.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticaExamenDisenoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
