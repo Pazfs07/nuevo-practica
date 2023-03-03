0a8305c948e646dd7238c1f7df351532bfb057aa
fercho-05 <fperezalpizar@gmail.com>
2023-03-02 20:09:04 -0600

    Errores

--- PracticaExamenDiseno 2/src/main/java/PracticaExamenDiseno/demo/entity/Concierto.java
+++ PracticaExamenDiseno 2/src/main/java/PracticaExamenDiseno/demo/entity/Concierto.java
@@ -5,6 +5,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import java.io.Serializable;
 
@@ -18,8 +20,20 @@ public class Concierto implements Serializable{
     
     private String costo;
     private String fecha;
-    private String MaxPersonas;
+    private int MaxPersonas;
     private String lugar;
+    
+    @ManyToOne
+    @JoinColumn(name="artista_id")
+    private Artista artista;
+
+    public Artista getArtista() {
+        return artista;
+    }
+
+    public void setArtista(Artista artista) {
+        this.artista = artista;
+    }
 
     public long getId() {
         return id;
@@ -45,11 +59,11 @@ public class Concierto implements Serializable{
         this.fecha = fecha;
     }
 
-    public String getMaxPersonas() {
+    public int getMaxPersonas() {
         return MaxPersonas;
     }
 
-    public void setMaxPersonas(String MaxPersonas) {
+    public void setMaxPersonas(int MaxPersonas) {
         this.MaxPersonas = MaxPersonas;
     }
 
