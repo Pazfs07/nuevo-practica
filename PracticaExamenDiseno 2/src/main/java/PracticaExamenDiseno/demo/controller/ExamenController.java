a8e9c62bcb2969a1d9bda6ee2b005b27e928211f
Darielk51 <94978506+Darielk51@users.noreply.github.com>
2023-03-07 21:10:41 -0600

    New 1.2

--- PracticaExamenDiseno 2/src/main/java/PracticaExamenDiseno/demo/controller/ExamenController.java
+++ PracticaExamenDiseno 2/src/main/java/PracticaExamenDiseno/demo/controller/ExamenController.java
@@ -12,6 +12,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class ExamenController {
@@ -42,5 +44,40 @@ public class ExamenController {
     public String guardarConcierto(@ModelAttribute Concierto concierto){
         conciertoService.saveConcert(concierto);
         return "redirect:/concierto";
+    }
+    //New code/ Revisar 
+    @Controller
+    @RequestMapping("/conciertos")
+    public class ExamenController {
+
+        private final IConciertoService conciertoService;
+        private final IArtistaService artistaService;
+
+        public ExamenController(IConciertoService conciertoService, IArtistaService artistaService) {
+            this.conciertoService = conciertoService;
+            this.artistaService = artistaService;
+        }
+
+        @RequestMapping(method = RequestMethod.GET)
+        public String listarConciertos(Model model) {
+            List<Concierto> conciertos = conciertoService.getAllConcert();
+            model.addAttribute("titulo", "Tabla Conciertos");
+            model.addAttribute("conciertos", conciertos);
+            return "conciertos";
+        }
+
+        @RequestMapping(value = "/nuevo", method = RequestMethod.GET)
+        public String crearConcierto(Model model) {
+            List<Artista> artistas = artistaService.listArtist();
+            model.addAttribute("concierto", new Concierto());
+            model.addAttribute("artistas", artistas);
+            return "crear";
+        }
+
+        @RequestMapping(value = "/guardar", method = RequestMethod.POST)
+        public String guardarConcierto(@ModelAttribute Concierto concierto) {
+            conciertoService.saveConcert(concierto);
+            return "redirect:/conciertos";
+        }
     }
 }
